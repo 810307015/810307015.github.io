4ef2b6342a4e02d44bae05a2e1c660fa15e4944d
zhangzhongming <zhangzhongming@baidu.com>
2024-04-18 15:28:11 +0800

    test

--- zamel-blog/src/config/index.ts
+++ zamel-blog/src/config/index.ts
@@ -1,20 +1,39 @@
+import banner0 from '@/assets/image/banner0.jpeg';
+import banner1 from '@/assets/image/banner1.jpeg';
+import banner2 from '@/assets/image/banner2.jpeg';
+import banner3 from '@/assets/image/banner3.jpeg';
+import banner4 from '@/assets/image/banner4.jpeg';
+import banner5 from '@/assets/image/banner5.jpeg';
+import banner6 from '@/assets/image/banner6.jpeg';
+import banner7 from '@/assets/image/banner7.jpeg';
+import banner8 from '@/assets/image/banner8.jpeg';
+import banner9 from '@/assets/image/banner9.jpeg';
+import banner10 from '@/assets/image/banner10.jpeg';
+import banner11 from '@/assets/image/banner11.jpeg';
+import banner12 from '@/assets/image/banner12.jpeg';
+import banner13 from '@/assets/image/banner13.jpeg';
+import banner14 from '@/assets/image/banner14.jpeg';
+import banner15 from '@/assets/image/banner15.jpeg';
+import banner16 from '@/assets/image/banner16.jpeg';
+import banner17 from '@/assets/image/banner17.jpeg';
+
 export const recordList: string[] = [
-    'https://810307015.github.io/image/banner0.jpeg',
-    'https://810307015.github.io/image/banner1.jpeg',
-    'https://810307015.github.io/image/banner2.jpeg',
-    'https://810307015.github.io/image/banner3.jpeg',
-    'https://810307015.github.io/image/banner4.jpeg',
-    'https://810307015.github.io/image/banner5.jpeg',
-    'https://810307015.github.io/image/banner6.jpeg',
-    'https://810307015.github.io/image/banner7.jpeg',
-    'https://810307015.github.io/image/banner8.jpeg',
-    'https://810307015.github.io/image/banner9.jpeg',
-    'https://810307015.github.io/image/banner10.jpeg',
-    'https://810307015.github.io/image/banner11.jpeg',
-    'https://810307015.github.io/image/banner12.jpeg',
-    'https://810307015.github.io/image/banner13.jpeg',
-    'https://810307015.github.io/image/banner14.jpeg',
-    'https://810307015.github.io/image/banner15.jpeg',
-    'https://810307015.github.io/image/banner16.jpeg',
-    'https://810307015.github.io/image/banner17.jpeg',
+    banner0,
+    banner1,
+    banner2,
+    banner3,
+    banner4,
+    banner5,
+    banner6,
+    banner7,
+    banner8,
+    banner9,
+    banner10,
+    banner11,
+    banner12,
+    banner13,
+    banner14,
+    banner15,
+    banner16,
+    banner17,
 ];
