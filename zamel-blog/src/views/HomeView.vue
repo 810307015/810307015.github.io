805b7aeb94388f07206db75ba161b87d9a1982e6
zhangzhongming <810307015@qq.com>
2026-03-02 16:21:53 +0800

    feat: implement rotating sphere photo wall interaction

--- zamel-blog/src/views/HomeView.vue
+++ zamel-blog/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
   <main class="container">
-    主要记录一些生活上和技术上的内容，想到了就会加进来，更多的是为了留个纪念
+    <p>主要记录一些生活上和技术上的内容，想到了就会加进来，更多的是为了留个纪念。</p>
+    <p>新功能：可以在 <RouterLink to="/picture-wall">图片墙</RouterLink> 页面查看所有图片素材并按名称快速筛选。</p>
   </main>
 </template>
